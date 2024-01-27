980cd9d214dbc3b5dd3c293a9345db9bb2a0a44e
iamabdulrafay <upwork8000@gmail.com>
2024-01-27 20:30:21 +0500

    first

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Specify the correct output directory
+  },
 });
